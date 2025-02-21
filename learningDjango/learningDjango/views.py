262992073702a0a61ffc969c21cf65d7b855effe
Manash Dev Bhatta <bmanash.a30@gmail.com>
2025-02-21 22:01:23 +0545

    create first app and jinja templates

--- learningDjango/learningDjango/views.py
+++ learningDjango/learningDjango/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Hello, World. This is Home page")
-    return render(request,'index.html')
+    return render(request,'website/index.html')
 
 def about(request):
     # return HttpResponse("Hello, World. This is About page")
-     return render(request,'about.html')
+     return render(request,'website/about.html')
 def contact(request):
     # return HttpResponse("Hello, World. This is Contact page")
-     return render(request,'contact.html')
+     return render(request,'website/contact.html')
