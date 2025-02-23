1f51b2dbf9b85d1ec21bbc7339436259db615419
Manash Dev Bhatta <bmanash.a30@gmail.com>
2025-02-23 21:07:33 +0545

    superuser and models

--- learningDjango/firstapp/models.py
+++ learningDjango/firstapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Firstapp(models.Model):
+    NAME_TYPE_CHOICE = [
+        ('MDB','Manash Dev Bhatta'),
+        ('SBB','Surya Bahadur Bhandari'),
+        ('KK','Karna Kumar'),
+    ]
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='firstappimg')
+    date_added = models.DateTimeField(default=timezone.now)
+    superheros = models.CharField(max_length=10,choices=NAME_TYPE_CHOICE)
+
+
+    def __str__(self):
+        return self.name
