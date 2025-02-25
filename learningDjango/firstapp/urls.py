8dea7e7c08ae50ac506351662cb55fb8cfc9c2eb
Manash Dev Bhatta <bmanash.a30@gmail.com>
2025-02-25 22:12:09 +0545

    Add URL pattern for firstapp_detail

--- learningDjango/firstapp/urls.py
+++ learningDjango/firstapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.firstapp, name=' firstapp'),
+    path('<int:item_id>/', views.firstapp_detail, name='firstapp_detail'),
    
 ]
--- learningDjango/firstapp/views.py
+++ learningDjango/firstapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import Firstapp
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def firstapp(request):
     f = Firstapp.objects.all()
     return render(request, 'firstapp/firstapp.html', {'f':f})
+
+
+def firstapp_detail(request, item_id):
+    first = get_object_or_404(Firstapp, pk=item_id)
+    return render(request,'firstapp/firstapp_detail.html',{'first':first})
+
