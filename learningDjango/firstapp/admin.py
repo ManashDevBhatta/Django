cb55e5a16ddb1afb0a9186b7fa4dcf735b459385
Manash Dev Bhatta <bmanash.a30@gmail.com>
2025-03-03 23:13:26 +0545

    Django relationship models

--- learningDjango/firstapp/admin.py
+++ learningDjango/firstapp/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Firstapp
+from .models import Firstapp, Review, Store, Profile
 
 # Register your models here.
-admin.site.register(Firstapp)
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 2
+
+
+class FirstappAdmin(admin.ModelAdmin):
+    list_display = ('name','superheros','date_added')
+    inlines = [ReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+    filter_horizontal = ('products',)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','bio')
+
+admin.site.register(Firstapp, FirstappAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Review)
--- learningDjango/firstapp/models.py
+++ learningDjango/firstapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -18,3 +19,34 @@ class Firstapp(models.Model):
 
     def __str__(self):
         return self.name
+    
+#One to Many Relationship
+class Review(models.Model):
+    fk = models.ForeignKey(Firstapp,on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField(default='')
+    date_added = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return f'{self.user.username}\'s review on {self.fk.name}'
+
+# Many to Many Relationship
+
+class Store(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    products = models.ManyToManyField(Firstapp, related_name='stores')
+
+    def __str__(self):
+        return self.name
+    
+# One to One Relationship
+
+class Profile(models.Model):
+    user = models.OneToOneField(Firstapp,on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(default='')
+
+    def __str__(self):
+        return f'{self.user.name}\'s profile'
